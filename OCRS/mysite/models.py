385a36787f3358f1614705a6226f0d9b68f86e66
aznanimedude <aznanimedude06@gmail.com>
2020-07-10 21:23:09 -0400

    update teacher model fields

--- OCRS/mysite/models.py
+++ OCRS/mysite/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,blank=True,null=True)
-    teacher_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
     def __str__(self):
         return self.teacher_name
 
